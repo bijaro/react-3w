8a1ad6d522b35ea41391fa1e139843bf5e6a2987
bijaro <lalejmehdi1@gmail.com>
2023-11-22 10:10:29 +0100

    first-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+
+import  Counter  from "./components/Counter";
+
+
+function App() {
+    return (
+        <div className="bg-red-400">
+            <Counter/>
+        </div>
+    );
+}
+
+export default App;
